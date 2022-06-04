ed288ffb002738fc562b3cbdb843ecd9a102dc6d
Sandy <48373918+Sandy10000@users.noreply.github.com>
2022-06-04 12:51:44 +0900

    Delete script.js

--- contents/demonstration/mscz2anim/BWV846Prelude/piano/video/script.js
+++
@@ -1,9 +0,0 @@
-import { keyvideo } from '../../../../../../../parts/module/keyvideo.js';
-// import { logvideo } from '../../../../../../../parts/module/logvideo.js';
-window.onkeydown = keydown;
-// logvideo();
-
-function keydown(e) {
-    // console.log("event keydown " + e.key, e.code, e.ctrlKey,  e.shiftKey, e.altKey, e.metaKey)
-    keyvideo(e);
-}
